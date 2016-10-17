ef0de8c4553bf22788a90f30db45a0b73303d598
justanton <justanton@users.noreply.github.com>
2016-10-17 23:25:24 +0200

    remove tokens

--- board/jobs/twitter.rb
+++ board/jobs/twitter.rb
@@ -4,10 +4,10 @@ require 'twitter'
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'I6gnJAEfeAXSUM1SgX6g9fa1f'
-  config.consumer_secret = 'oU8GRirPaZpY1IPEXBHxAo1WAQ7IxBMoukINkBDaOLvvNif3TA'
-  config.access_token = '100336165-EgOJskqXq3VJojRhZAzGRhFjKM8RNgN3ILqzSR3z'
-  config.access_token_secret = 'r2EyV31hm0wUNypJ9WO1ZQi63MyVzDzgf7imoHfETBYgp'
+  config.consumer_key = 'CONSUMER KEY'
+  config.consumer_secret = 'CONSUMER_SECRET'
+  config.access_token = 'CONSUMER_TOKEN'
+  config.access_token_secret = 'TOKEN_SECRET'
 end
 
 search_term = URI::encode('#alexajacobshack')
